f0180963fe15b9027a96b49cb464016597635aea
baby11009 <vohuythanh2003@gmail.com>
2024-11-05 20:05:50 +0700

    update 5/11/2024

---
+++ middlewares/get-account-info.js
@@ -0,0 +1,29 @@
+const { User } = require("../models");
+const jwt = require("jsonwebtoken");
+
+const getAccountInfo = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(payload.userId).select("-password");
+
+    if (user.confirmed === false) {
+      return next(new BadRequestError("Account not confirmed"));
+    }
+
+    // if (payload.exp * 1000 < Date.now()) {
+    //   return next(new BadRequestError("Token expired"));
+    // }
+
+    req.user = {
+      userId: payload.userId,
+      name: payload.username,
+      role: user.role,
+    };
+  }
+
+  next();
+};
+module.exports = getAccountInfo;
